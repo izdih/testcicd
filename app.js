7ede82684c483b8dd278c2ee8777fa2bbbd46bd3
izdihar <izdihar@mail.com>
2024-02-12 15:40:20 +0100

    fix pipeline

--- app.js
+++ app.js
@@ -2,15 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send('Bonjour...!')
+})
 
-  app.get('/auth', (req, res) => {
-    res.send('user logged in!')
-  })
-  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
